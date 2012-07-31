d479e39499fd9dae7c56dd8c6784430968a09234
Gabor Pali <pali.gabor@gmail.com>
2012-07-31 16:15:14 +0200

    Remove extra linebreaks and set a smaller number for the Fibonacci function :-)

--- packages/mirage-test/main.ml
+++ packages/mirage-test/main.ml
@@ -4,13 +4,13 @@ let rec fib n =
   if n < 2 then 1 else fib (n - 1) + fib (n - 2)
 
 let f () =
-  prerr_endline "Sorry, no in-kernel Mirage today!\n";
-  prerr_endline "And there is a second message...\n";
-  prerr_endline "Finally a third one.\n";
-  prerr_endline "Let's do some Fibonacci!\n";
-  fib 100;
+  prerr_endline "Sorry, no in-kernel Mirage today!";
+  prerr_endline "And there is a second message...";
+  prerr_endline "Finally a third one.";
+  prerr_endline "Let's do some Fibonacci!";
+  fib 32;
   true
 
 let _ =
-  prerr_endline "This message comes somewhere from the body.\n";
+  prerr_endline "This message comes somewhere from the body.";
   Callback.register "OS.Main.run" f
