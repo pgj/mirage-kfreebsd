8e99c8d87009b9379bfe28e6672e376430fc6822
Gabor Pali <pali.gabor@gmail.com>
2012-08-02 12:46:49 +0200

    Use implicit flush in printf calls.

--- packages/mirage-test/main.ml
+++ packages/mirage-test/main.ml
@@ -1,19 +1,17 @@
 
 let rec fib n =
-  Printf.printf "[n = %d]" n;
+  Printf.printf "[n = %d]%!" n;
   if n < 2 then 1 else fib (n - 1) + fib (n - 2)
 
 let f () =
-  Printf.printf "Sorry, no in-kernel Mirage today!\n";
-  Printf.printf "And there is a second message...\n";
-  Printf.printf "Finally a third one.\n";
-  Printf.printf "Let's do some Fibonacci!\n";
-  fib 32;
-  Printf.printf "\n";
-  flush stdout;
+  Printf.printf "Sorry, no in-kernel Mirage today!\n%!";
+  Printf.printf "And there is a second message...\n%!";
+  Printf.printf "Finally a third one.\n%!";
+  Printf.printf "Let's do some Fibonacci!\n%!";
+  ignore (fib 10);
+  Printf.printf "\n%!";
   true
 
 let _ =
-  Printf.printf "This message comes somewhere from the body.\n";
-  flush stdout;
+  Printf.printf "This message comes somewhere from the body.\n%!";
   Callback.register "OS.Main.run" f
