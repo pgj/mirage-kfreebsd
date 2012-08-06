fdbe6f0000a082a2732fc867a2addf9618af27bf
Gabor Pali <pali.gabor@gmail.com>
2012-08-06 17:15:29 +0200

    The allocation test now works with larger sizes without problems.

--- packages/mirage-test/regress/perf/alloc.ml
+++ packages/mirage-test/regress/perf/alloc.ml
@@ -10,7 +10,6 @@ let with_time i label fn =
 
 let main () =
   let sizes = [1; 500; 2500; 5000; 10000; 15000; 30000; 50000; 100000; 250000 ] in
-  let sizes = [1; 500; 2500; 5000; 10000; 15000; ] in
   List.iter (fun sz ->
     with_time sz sz (fun () ->
       for i = 0 to 1000000 do
